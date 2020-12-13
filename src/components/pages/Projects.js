01cf7d5f169c10499690e1b76305d0cef756b437
pson0007 <ping.song@monash.edu>
2020-12-13 16:43:53 +1100

    update

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import './projects.scss'
 import Peerview from './../../assets/peerview.png'
+import Checkin from './../../assets/checkin.png'
 
 const Projects = (props) => {
   console.log(props)
   return (
     <div className="projectsContainer">
       <div className="projects">
-        <div className="projectContainer">
+        <div className="projectRight">
           <div className="projectTitle">
             <span>PeerView</span>
           </div>
-          <div>
+          <div className="projectNumber">
             <span>01</span>
+
             <img src={Peerview} alt="Peer View" className="projectImg"></img>
           </div>
-          <div>
+          <div className="projectDesc">
             <span>
               This product is designed and developed for academic staff to
               request reviews from their peers. It digitizes the experience of
               the review process.
+            </span>
+          </div>
+        </div>
+        <div className="projectLeft">
+          <div className="projectTitle">
+            <span>Check in. Keep Safe.</span>
+          </div>
+          <div className="projectNumber">
+            <img src={Checkin} alt="Check In" className="projectImg"></img>
+            <span>02</span>
+          </div>
+          <div className="projectDesc">
+            <span>
+              As COVID-19 began to accelerate its spread in Spring 2020, this
+              app was born to support Monash University community to return to
+              campus safely.
             </span>
           </div>
         </div>
